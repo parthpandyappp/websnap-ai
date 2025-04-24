511c0bf761046f803d44d73e65bf1798a8c3ba19
parthpandyappp <parthpandyappp@gmail.com>
2025-04-24 15:52:32 +0530

    chore: use remote asset for loading

--- src/components/loader/index.tsx
+++ src/components/loader/index.tsx
@@ -1,9 +1,8 @@
 export const Loader = ({ message }: { message: string }) => (
   <div className="flex flex-col justify-center items-center w-full h-full">
-    <img
-      src="/loading.gif"
-      alt="Loading animation"
-      className="w-200 h-200 mb-4"
+    <iframe
+      src="https://lottie.host/embed/7a59336f-b9ae-4ba5-b330-0575bf49fd00/eUU3bDkmFL.lottie"
+      title="Loading animation"
     />
     <p className="text-center text-sm text-white font-semibold">{message}</p>
   </div>
